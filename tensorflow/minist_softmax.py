9aa0a01387f68adf468077b0c43cd573536c0557
ycw <yucongwei@geekfinancer.com>
2017-07-03 19:16:42 +0800

    tensorflow practice

---
+++ tensorflow/minist_softmax.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+minist = input_data.read_data_sets('/home/ycw/Downloads/MINIST', one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+w = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.matmul(x, w) + b
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+if __name__ == '__main__':
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+
+    for _ in range(1000):
+        batch_xs, batch_ys = minist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.arg_max(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: minist.test.images, y_: minist.test.labels}))
