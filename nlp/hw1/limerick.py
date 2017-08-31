9133a31c6b77823f7a8843014810343245bac42d
aforwardz <aforwardzyu@gmail.com>
2017-09-01 00:34:32 +0800

    limerick num of syllables

--- nlp/hw1/limerick.py
+++ nlp/hw1/limerick.py
@@ -2,10 +2,11 @@
 import argparse
 import sys
 import codecs
+
 if sys.version_info[0] == 2:
-  from itertools import izip
+    from itertools import izip
 else:
-  izip = zip
+    izip = zip
 from collections import defaultdict as dd
 import re
 import os.path
@@ -25,37 +26,36 @@ scriptdir = os.path.dirname(os.path.abspath(__file__))
 reader = codecs.getreader('utf8')
 writer = codecs.getwriter('utf8')
 
+
 def prepfile(fh, code):
-  if type(fh) is str:
-    fh = open(fh, code)
-  ret = gzip.open(fh.name, code if code.endswith("t") else code+"t") if fh.name.endswith(".gz") else fh
-  if sys.version_info[0] == 2:
-    if code.startswith('r'):
-      ret = reader(fh)
-    elif code.startswith('w'):
-      ret = writer(fh)
-    else:
-      sys.stderr.write("I didn't understand code "+code+"\n")
-      sys.exit(1)
-  return ret
+    if type(fh) is str:
+        fh = open(fh, code)
+    ret = gzip.open(fh.name, code if code.endswith("t") else code + "t") if fh.name.endswith(".gz") else fh
+    if sys.version_info[0] == 2:
+        if code.startswith('r'):
+            ret = reader(fh)
+        elif code.startswith('w'):
+            ret = writer(fh)
+        else:
+            sys.stderr.write("I didn't understand code " + code + "\n")
+            sys.exit(1)
+    return ret
+
 
 def addonoffarg(parser, arg, dest=None, default=True, help="TODO"):
-  ''' add the switches --arg and --no-arg that set parser.arg to true/false, respectively'''
-  group = parser.add_mutually_exclusive_group()
-  dest = arg if dest is None else dest
-  group.add_argument('--%s' % arg, dest=dest, action='store_true', default=default, help=help)
-  group.add_argument('--no-%s' % arg, dest=dest, action='store_false', default=default, help="See --%s" % arg)
-
+    ''' add the switches --arg and --no-arg that set parser.arg to true/false, respectively'''
+    group = parser.add_mutually_exclusive_group()
+    dest = arg if dest is None else dest
+    group.add_argument('--%s' % arg, dest=dest, action='store_true', default=default, help=help)
+    group.add_argument('--no-%s' % arg, dest=dest, action='store_false', default=default, help="See --%s" % arg)
 
 
 class LimerickDetector:
-
     def __init__(self):
         """
         Initializes the object to have a pronunciation dictionary available
         """
         self._pronunciations = nltk.corpus.cmudict.dict()
-
 
     def num_syllables(self, word):
         """
@@ -65,8 +65,12 @@ class LimerickDetector:
         """
 
         # TODO: provide an implementation!
+        pronounce_2_list = self._pronunciations.get(word.lower(), [[]])
+        shorter_pronounce = min(pronounce_2_list, key=len)
 
-        return 1
+        # len(list(filter(lambda p: p[-1].isdigit(), short_pronounce)))
+        return list(map(lambda p: p[-1].isdigit(), shorter_pronounce)).count(True) \
+            if shorter_pronounce else 1
 
     def rhymes(self, a, b):
         """
@@ -105,26 +109,26 @@ class LimerickDetector:
 
 # The code below should not need to be modified
 def main():
-  parser = argparse.ArgumentParser(description="limerick detector. Given a file containing a poem, indicate whether that poem is a limerick or not",
-                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  addonoffarg(parser, 'debug', help="debug mode", default=False)
-  parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file")
-  parser.add_argument("--outfile", "-o", nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="output file")
+    parser = argparse.ArgumentParser(
+        description="limerick detector. Given a file containing a poem, indicate whether that poem is a limerick or not",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    addonoffarg(parser, 'debug', help="debug mode", default=False)
+    parser.add_argument("--infile", "-i", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file")
+    parser.add_argument("--outfile", "-o", nargs='?', type=argparse.FileType('w'), default=sys.stdout,
+                        help="output file")
 
+    try:
+        args = parser.parse_args()
+    except IOError as msg:
+        parser.error(str(msg))
 
+    infile = prepfile(args.infile, 'r')
+    outfile = prepfile(args.outfile, 'w')
 
+    ld = LimerickDetector()
+    lines = ''.join(infile.readlines())
+    outfile.write("{}\n-----------\n{}\n".format(lines.strip(), ld.is_limerick(lines)))
 
-  try:
-    args = parser.parse_args()
-  except IOError as msg:
-    parser.error(str(msg))
-
-  infile = prepfile(args.infile, 'r')
-  outfile = prepfile(args.outfile, 'w')
-
-  ld = LimerickDetector()
-  lines = ''.join(infile.readlines())
-  outfile.write("{}\n-----------\n{}\n".format(lines.strip(), ld.is_limerick(lines)))
 
 if __name__ == '__main__':
-  main()
+    main()
