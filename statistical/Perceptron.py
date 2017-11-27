6fd17d3642dbef2bfc6f0de4427f55d3d38cd19b
Aforwardz <aforwardzyu@gmail.com>
2017-11-27 20:08:59 +0800

    2017-11-27 record and Perceptron Model

--- statistical/Perceptron.py
+++ statistical/Perceptron.py
@@ -1,26 +1,54 @@
 # coding: utf-8
 # 赶只鸡 算法
+import os
 from sklearn import datasets
-import numpy
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 # 构造数据
 # 参数的意思：100个例子，2种特征，1种输出，噪声的大小为2
-n_s, n_f, n_c = 100, 2, 1
-X, y = datasets.make_multilabel_classification(
-    n_samples=n_s, n_features=n_f, n_classes=n_c)
+# n_s, n_f, n_c, noise = 100, 5, 2, 2
+# X, y = datasets.make_sparse_uncorrelated(
+#     n_samples=n_s, n_features=n_f)
+
+X = [[1, 2], [2, .5], [1.5, .8], [1, .2], [2, -0.4], [1.5, 0]]
+y = [1, 1, 1, -1, -1, -1]
 # print(X, y)
-X_matrix = numpy.asarray(X)  # 将list转成numpy array
-y_matrix = numpy.asarray(y)
+X_matrix = np.matrix(X)  # 将list转成numpy array
+y_matrix = np.matrix(y)
 print(X_matrix.shape, y_matrix.shape)
 
-W = numpy.zeros((n_c, n_f))
-b = numpy.zeros((n_s, 1))
+W = np.zeros((2, 1))
+b = 0  # np.zeros((6, 1))
 LAP = 1  # 拉普拉斯平滑
 
 # percetron model
-percetron = lambda x: 1 if x >= 0 else 0
+percetron = lambda x: 1 if x >= 0 else -1
 
 
-# plt.plot(X, y, 'o')
-# plt.show()
+def update(x, l):
+    global W, b
+    W = W + (LAP*l*x).T
+    b = b + LAP*l
+    print('update to: --\n', W, '\n-- and ', b)
+
+
+def check():
+    flag = False
+    for d, c in zip(X, y):
+        d = np.matrix(d)
+        r = np.matmul(d, W)
+        if c * percetron(r.prod()+b) < 0:
+            flag = True
+            update(d, c)
+    if not flag:
+        print('weight and bias: ', W, b)
+        os._exit(0)
+
+if __name__ == '__main__':
+    for i in range(1000):
+        print('Now is the %s step' % i)
+        check()
+
+    plt.plot(X, 'o')
+    plt.show()
